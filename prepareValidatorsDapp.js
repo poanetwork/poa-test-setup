828b2ad69401cd8a18dd360da58086c19ecc2e5b
viktor <baranov.viktor.27@gmail.com>
2018-02-21 15:52:53 +0300

    Fixes to run validator's node

--- prepareValidatorsDapp.js
+++ prepareValidatorsDapp.js
@@ -9,18 +9,20 @@ function main() {
 	const pathToAddressesJSON = `${constants.pathToContractRepo}/${constants.addressesSourceFile}`;
 	const addresses = JSON.parse(fs.readFileSync(pathToAddressesJSON));
 
+	let moc = JSON.parse(fs.readFileSync(`${constants.mocKeysFolder}moc.key`, 'utf8'));
+
 	const addition = `
 		const local = { 
 			"METADATA_ADDRESS": "${addresses.KEYS_MANAGER_ADDRESS}",
 			"KEYS_MANAGER_ADDRESS": "${addresses.KEYS_MANAGER_ADDRESS}",
-			"POA_ADDRESS": '',
-			"MOC": ''
+			"POA_ADDRESS": '${constants.poaNetworkConsensusContractAddress}',
+			"MOC": '${moc.address}'
 		};
 	`
 
 	let dappAddresses = `${constants.pathToValidatorsDAppRepo}/src/contracts/addresses.js`;
 	let addressesFromDapp = fs.readFileSync(dappAddresses, 'utf8');
-	let lastImport = `import helpers from "./helpers"`;
+	let lastImport = `import helpers from './helpers'`;
 	addressesFromDapp = addressesFromDapp.replace(lastImport, lastImport + addition)
 	addressesFromDapp = addressesFromDapp.replace('resolve({addresses: json', 'resolve({addresses: local')
 
